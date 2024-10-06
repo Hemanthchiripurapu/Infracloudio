c6517f5f8731c7ab6b41ee9f6c942d2aaad4ced6
Hemanthchiripurapu <147477439+Hemanthchiripurapu@users.noreply.github.com>
2024-10-06 18:21:14 +0530

    Update gencsv.sh

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -4,10 +4,9 @@ start=$1
 end=$2
 filename="inputFile"
 
-# Clear the file if it exists
+
 > $filename
 
-# Generate the CSV file with index and random number
 for ((i=start; i<=end; i++))
 do
   echo "$i, $((RANDOM % 1000))" >> $filename
